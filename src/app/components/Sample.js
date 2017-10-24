5d638ff33e8e3e825d96f8ae59d637bdc735380f
Jomar Santos <jomaroliversantos@gmail.com>
2017-10-23 22:56:05 -0700

    add react

---
+++ src/app/components/Sample.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Sample extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <h1>hello</h1>
+    );
+  }
+}
+export default Sample;
--- src/app/main.js
+++ src/app/main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Sample from './components/Sample';
+
+document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(Sample),
+    document.getElementById('mount')
+  );
+});
